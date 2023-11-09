3b6560d17375f8c906182172fffb8388b107cecf
Abdurahman AYDIN <hariysu@gmail.com>
2023-11-09 16:52:27 +0300

    Finishes closure example

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,83 @@
 import UIKit
 
-var greeting = ""
+func firstCalculator (n1: Int, n2: Int) -> Int {
+    return n1 * n2
+}
+
+// We took "(no1: Int, no2: Int) -> Int" and named as operation(removed parameter names)
+func calculator (n1: Int, n2: Int, operation: (Int, Int) -> Int) -> Int {
+    return operation(n1, n2)
+}
+
+func add (no1: Int, no2: Int) -> Int {
+    return no1 + no2
+}
+
+func multiply (no1: Int, no2: Int) -> Int {
+    return no1 * no2
+}
+
+//Closure form of multiply method
+//{ (no1: Int, no2: Int) -> Int in
+//    return no1 * no2
+//}
+
+//calculator(n1 :2, n2: 3, operation: add)
+calculator(n1 :2, n2: 3, operation: multiply )
+
+// form 0
+calculator(n1 :2, n2: 3, operation: { (no1: Int, no2: Int) -> Int in
+    return no1 * no2
+})
+
+// form 1
+calculator(n1 :2, n2: 3, operation: { (no1, no2) -> Int in
+    return no1 * no2
+})
+
+// form 2
+calculator(n1 :2, n2: 3, operation: { (no1, no2)  in
+    return no1 * no2
+})
+
+// form 3
+calculator(n1 :2, n2: 3, operation: { (no1, no2)  in no1 * no2})
+
+// form 4
+calculator(n1 :2, n2: 3, operation: {$0 * $1})
+
+// form 5 (trailing closure)
+calculator(n1 :2, n2: 3) { $0 * $1}
+
+let result = calculator(n1 :2, n2: 3) {$0 * $1}
+print(result)
+
+let array = [6,2,3,9,4,1]
+
+func addOne (n1: Int) -> Int {
+    return n1 + 1
+}
+
+array.map(addOne)
+
+//form1
+array.map({(n1) -> Int in
+    return n1 + 1
+})
+
+//form2
+array.map({(n1) in
+    return n1 + 1
+})
+
+//form3
+array.map({(n1) in n1 + 1
+})
+
+//form4
+array.map({$0 + 1})
+
+//form5 (trailing closure)
+array.map{$0 + 1}
+
+
---
+++ README.md
@@ -0,0 +1,34 @@
+# Closures-Section-13
+
+- closures are anonymous functions, 
+
+![Closures Section 13](Documentation/ss1.png)
+
+
+- you can actually pass a function as an input to another function
+- you can also have a function as the output of another function
+- you can consider functions as named closures
+
+![Closures Section 13](Documentation/ss2.png)
+
+
+Step-1 
+
+![Closures Section 13](Documentation/ss3.png)
+
+
+Step-2
+
+![Closures Section 13](Documentation/ss4.png)
+
+
+Closure Scheme
+![Closures Section 13](Documentation/ss5.png)
+
+
+
+
+
+
+
+
